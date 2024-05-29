85d608402ce58654e1f48fcbd8cb09def3358334
Josh McKinney <joshka@users.noreply.github.com>
2024-05-29 15:52:36 -0700

    Animate the background color when changing from positive to negative

--- examples/demo.rs
+++ examples/demo.rs
@@ -11,9 +11,9 @@ use bevy_ratatui::{
 };
 use crossterm::event::KeyCode;
 use ratatui::{
-    buffer,
+    buffer::Buffer,
     layout::Rect,
-    style::{Color, Stylize},
+    style::{Color, Style},
     text::Line,
     widgets::WidgetRef,
 };
@@ -29,6 +29,10 @@ fn main() {
         .add_event::<CounterEvent>()
         .init_state::<AppState>()
         .add_systems(Update, update_counter_system)
+        .add_systems(OnEnter(AppState::Negative), start_background_color_timer)
+        .add_systems(OnEnter(AppState::Positive), start_background_color_timer)
+        .add_systems(Update, background_color_system)
+        .init_resource::<BackgroundColor>()
         .run();
 }
 
@@ -37,13 +41,13 @@ fn ui_system(
     frame_count: Res<FrameCount>,
     counter: Res<Counter>,
     app_state: Res<State<AppState>>,
+    bg_color: Res<BackgroundColor>,
 ) -> color_eyre::Result<()> {
     context.draw(|frame| {
         let area = frame.size();
         let frame_count = Line::from(format!("Frame Count: {}", frame_count.0)).right_aligned();
-
+        frame.render_widget(bg_color.as_ref(), area);
         frame.render_widget(frame_count, area);
-
         frame.render_widget(counter.as_ref(), area);
         frame.render_widget(app_state.get(), area)
     })?;
@@ -114,7 +118,7 @@ fn update_counter_system(
 }
 
 impl WidgetRef for Counter {
-    fn render_ref(&self, area: Rect, buf: &mut buffer::Buffer) {
+    fn render_ref(&self, area: Rect, buf: &mut Buffer) {
         let counter = format!("Counter: {}", self.0);
         Line::from(counter).render_ref(area, buf);
     }
@@ -128,11 +132,82 @@ enum AppState {
 }
 
 impl WidgetRef for AppState {
-    fn render_ref(&self, area: Rect, buf: &mut buffer::Buffer) {
-        let (state, color) = match self {
-            AppState::Negative => ("Negative", Color::Red),
-            AppState::Positive => ("Positive", Color::Green),
+    fn render_ref(&self, area: Rect, buf: &mut Buffer) {
+        let state = match self {
+            AppState::Negative => "Negative",
+            AppState::Positive => "Positive",
         };
-        Line::from(state).centered().bg(color).render_ref(area, buf);
+        Line::from(state).centered().render_ref(area, buf);
     }
+}
+
+#[derive(Debug, Component, Deref, DerefMut)]
+struct ColorChangeTimer {
+    #[deref]
+    timer: Timer,
+    start_color: Color,
+}
+
+fn start_background_color_timer(mut commands: Commands, bg_color: Res<BackgroundColor>) {
+    commands.spawn(ColorChangeTimer {
+        timer: Timer::from_seconds(1.0, TimerMode::Once),
+        start_color: bg_color.0,
+    });
+}
+
+#[derive(Debug, Resource, Deref, DerefMut)]
+struct BackgroundColor(Color);
+
+impl Default for BackgroundColor {
+    fn default() -> Self {
+        BackgroundColor(Color::Rgb(0, 0, 0))
+    }
+}
+
+impl WidgetRef for BackgroundColor {
+    fn render_ref(&self, area: Rect, buf: &mut Buffer) {
+        buf.set_style(area, Style::new().bg(self.0));
+    }
+}
+
+/// Change the background color over time when the app state changes from negative to positive
+/// or vice versa.
+fn background_color_system(
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut ColorChangeTimer)>,
+    app_state: Res<State<AppState>>,
+    mut commands: Commands,
+    mut bg_color: ResMut<BackgroundColor>,
+) {
+    let Ok((entity, mut timer)) = query.get_single_mut() else {
+        return;
+    };
+    timer.tick(time.delta());
+    let end_color = match app_state.get() {
+        AppState::Negative => Color::Rgb(255, 0, 0),
+        AppState::Positive => Color::Rgb(0, 255, 0),
+    };
+    bg_color.0 = interpolate(timer.start_color, end_color, timer.fraction())
+        .expect("only works for rgb colors");
+    if timer.just_finished() {
+        commands.entity(entity).despawn();
+    }
+}
+
+/// Interpolate between two colors based on the fraction
+///
+/// This is just a simple linear interpolation between the two colors (a real implementation would
+/// use a color space that is perceptually uniform).
+fn interpolate(start: Color, end: Color, fraction: f32) -> Option<Color> {
+    let Color::Rgb(start_red, start_green, start_blue) = start else {
+        return None;
+    };
+    let Color::Rgb(end_red, end_green, end_blue) = end else {
+        return None;
+    };
+    Some(Color::Rgb(
+        (start_red as f32 + (end_red as f32 - start_red as f32) * fraction) as u8,
+        (start_green as f32 + (end_green as f32 - start_green as f32) * fraction) as u8,
+        (start_blue as f32 + (end_blue as f32 - start_blue as f32) * fraction) as u8,
+    ))
 }
