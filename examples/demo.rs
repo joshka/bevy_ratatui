970041663ab9b09669fd57a3bc8c8276d1b48c04
Josh McKinney <joshka@users.noreply.github.com>
2024-05-29 05:48:03 -0700

    demonstrate how bevy makes changing state elsewhere in the app easy
    Some context: https://forum.ratatui.rs/t/how-do-i-represent-application-state-ergonomically/54/13

--- examples/demo.rs
+++ examples/demo.rs
@@ -10,26 +10,42 @@ use bevy_ratatui::{
     error::exit_on_error, event::KeyEvent, terminal::RatatuiContext, RatatuiPlugins,
 };
 use crossterm::event::KeyCode;
-use ratatui::text::Line;
+use ratatui::{
+    buffer,
+    layout::Rect,
+    style::{Color, Stylize},
+    text::Line,
+    widgets::WidgetRef,
+};
 
 fn main() {
     let frame_rate = Duration::from_secs_f64(1. / 60.);
     App::new()
         .add_plugins(RatatuiPlugins)
         .add_plugins(MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(frame_rate)))
+        .add_systems(PreUpdate, keyboard_input_system.pipe(exit_on_error))
         .add_systems(Update, ui_system.pipe(exit_on_error))
-        .add_systems(Update, keyboard_input_system.pipe(exit_on_error))
+        .init_resource::<Counter>()
+        .add_event::<CounterEvent>()
+        .init_state::<AppState>()
+        .add_systems(Update, update_counter_system)
         .run();
 }
 
 fn ui_system(
     mut context: ResMut<RatatuiContext>,
     frame_count: Res<FrameCount>,
+    counter: Res<Counter>,
+    app_state: Res<State<AppState>>,
 ) -> color_eyre::Result<()> {
     context.draw(|frame| {
-        frame.render_widget("Hello World!", frame.size());
+        let area = frame.size();
         let frame_count = Line::from(format!("Frame Count: {}", frame_count.0)).right_aligned();
-        frame.render_widget(frame_count, frame.size())
+
+        frame.render_widget(frame_count, area);
+
+        frame.render_widget(counter.as_ref(), area);
+        frame.render_widget(app_state.get(), area)
     })?;
     Ok(())
 }
@@ -37,6 +53,7 @@ fn ui_system(
 fn keyboard_input_system(
     mut events: EventReader<KeyEvent>,
     mut app_exit: EventWriter<AppExit>,
+    mut counter_events: EventWriter<CounterEvent>,
 ) -> color_eyre::Result<()> {
     for event in events.read() {
         match event.code {
@@ -46,8 +63,76 @@ fn keyboard_input_system(
             KeyCode::Char('p') => {
                 panic!("Panic!");
             }
+            KeyCode::Left => {
+                counter_events.send(CounterEvent::Decrement);
+            }
+            KeyCode::Right => {
+                counter_events.send(CounterEvent::Increment);
+            }
             _ => {}
         }
     }
     Ok(())
+}
+
+#[derive(Default, Resource, Debug, Deref, DerefMut)]
+struct Counter(i32);
+
+impl Counter {
+    fn decrement(&mut self) {
+        self.0 -= 1;
+    }
+
+    fn increment(&mut self) {
+        self.0 += 1;
+    }
+}
+
+#[derive(Debug, Clone, Copy, Event, PartialEq, Eq)]
+enum CounterEvent {
+    Increment,
+    Decrement,
+}
+
+fn update_counter_system(
+    mut counter: ResMut<Counter>,
+    mut events: EventReader<CounterEvent>,
+    mut app_state: ResMut<NextState<AppState>>,
+) {
+    for event in events.read() {
+        match event {
+            CounterEvent::Increment => counter.increment(),
+            CounterEvent::Decrement => counter.decrement(),
+        }
+        // demonstrates changing something in the app state based on the counter value
+        if counter.0 < 0 {
+            app_state.set(AppState::Negative);
+        } else {
+            app_state.set(AppState::Positive);
+        }
+    }
+}
+
+impl WidgetRef for Counter {
+    fn render_ref(&self, area: Rect, buf: &mut buffer::Buffer) {
+        let counter = format!("Counter: {}", self.0);
+        Line::from(counter).render_ref(area, buf);
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
+enum AppState {
+    Negative,
+    #[default]
+    Positive,
+}
+
+impl WidgetRef for AppState {
+    fn render_ref(&self, area: Rect, buf: &mut buffer::Buffer) {
+        let (state, color) = match self {
+            AppState::Negative => ("Negative", Color::Red),
+            AppState::Positive => ("Positive", Color::Green),
+        };
+        Line::from(state).centered().bg(color).render_ref(area, buf);
+    }
 }
