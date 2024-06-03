aae39c8d1c5b1e665849c3fe50e6229cff268ef1
Josh McKinney <joshka@users.noreply.github.com>
2024-06-03 13:36:09 -0700

    feat: add rest of crossterm events

--- src/event.rs
+++ src/event.rs
@@ -25,7 +25,8 @@ use std::time::Duration;
 
 use bevy::{app::AppExit, prelude::*};
 use color_eyre::Result;
-use crossterm::event::{self, KeyCode, KeyEventKind, KeyModifiers};
+use crossterm::event::{self, Event::Key, KeyCode, KeyEventKind, KeyModifiers};
+use ratatui::layout::Size;
 
 use crate::error::exit_on_error;
 
@@ -42,28 +43,73 @@ impl Plugin for EventPlugin {
     }
 }
 
-/// Wrapper around `crossterm::event::KeyEvent`.
+/// An event that is sent whenever an event is read from crossterm.
+#[derive(Debug, Deref, Event, PartialEq, Eq, Clone, Hash)]
+pub struct CrosstermEvent(pub event::Event);
+
+/// An event that is sent whenever a key event is read from crossterm.
 #[derive(Debug, Deref, Event, PartialEq, Eq, Clone, Hash)]
 pub struct KeyEvent(pub event::KeyEvent);
+
+/// An event that is sent whenever a mouse event is read from crossterm.
+#[derive(Debug, Clone, Copy, Event, PartialEq, Eq)]
+pub struct MouseEvent(pub event::MouseEvent);
+
+/// An event that is sent when the terminal gains or loses focus.
+#[derive(Debug, Clone, Copy, Event, PartialEq, Eq)]
+pub enum FocusEvent {
+    Gained,
+    Lost,
+}
+
+/// An event that is sent when the terminal is resized.
+#[derive(Debug, Clone, Copy, Event, PartialEq, Eq, Deref)]
+pub struct ResizeEvent(pub Size);
+
+/// An event that is sent when text is pasted into the terminal.
+#[derive(Debug, Clone, Event, PartialEq, Eq, Deref)]
+pub struct PasteEvent(pub String);
 
 /// System that reads events from crossterm and sends them to the `KeyEvent` event.
 ///
 /// This system reads events from crossterm and sends them to the `KeyEvent` event. It also sends
 /// an `AppExit` event when `Ctrl+C` is pressed.
 pub fn crossterm_event_system(
-    mut exit: EventWriter<AppExit>,
+    mut events: EventWriter<CrosstermEvent>,
     mut keys: EventWriter<KeyEvent>,
+    mut mouse: EventWriter<MouseEvent>,
+    mut focus: EventWriter<FocusEvent>,
+    mut paste: EventWriter<PasteEvent>,
+    mut resize: EventWriter<ResizeEvent>,
+    mut exit: EventWriter<AppExit>,
 ) -> Result<()> {
     while event::poll(Duration::ZERO)? {
-        match event::read()? {
-            event::Event::Key(event) if event.kind == KeyEventKind::Press => {
+        let event = event::read()?;
+        match event {
+            Key(event) if event.kind == KeyEventKind::Press => {
                 if event.modifiers == KeyModifiers::CONTROL && event.code == KeyCode::Char('c') {
                     exit.send_default();
                 }
                 keys.send(KeyEvent(event));
             }
+            event::Event::FocusLost => {
+                focus.send(FocusEvent::Lost);
+            }
+            event::Event::FocusGained => {
+                focus.send(FocusEvent::Gained);
+            }
+            event::Event::Mouse(event) => {
+                mouse.send(MouseEvent(event));
+            }
+            event::Event::Paste(ref s) => {
+                paste.send(PasteEvent(s.clone()));
+            }
+            event::Event::Resize(columns, rows) => {
+                resize.send(ResizeEvent(Size::new(columns, rows)));
+            }
             _ => {}
         }
+        events.send(CrosstermEvent(event));
     }
     Ok(())
 }
